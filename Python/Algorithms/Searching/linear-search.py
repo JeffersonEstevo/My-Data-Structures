6c7872c31a0d28d7bf8fd682483c1c3521d5537d
Jefferson Estevo <62241112+JeffersonEstevo@users.noreply.github.com>
2024-05-05 00:13:57 -0300

    Update linear-search.py

--- Python/Algorithms/Searching/linear-search.py
+++ Python/Algorithms/Searching/linear-search.py
@@ -13,6 +13,7 @@ Best Case: O(1)
 Average Case: O(n)
 Worst Case: O(n)
 """
+
 def linearSearch(arr,target):
     for i in range(len(arr)): # traversing the list
         if arr[i] == target: # comparing the list element with the target value

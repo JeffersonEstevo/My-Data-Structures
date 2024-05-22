e88371a1b040086c1dec20a1ec8691deb6ceaf12
Jefferson Estevo <62241112+JeffersonEstevo@users.noreply.github.com>
2024-05-21 23:10:45 -0300

    Update Lcs.cpp

--- C++/Algorithms/DynamicPrograming/Lcs.cpp
+++ C++/Algorithms/DynamicPrograming/Lcs.cpp
@@ -5,30 +5,26 @@
 
 using namespace std;
 
-int lcs(string X,string Y,int n,int m)
-{
+int lcs(string X,string Y,int n,int m){
     //Initializing the two dimensional table
     int t[m+1][n+1];
-    for(int i=0;i<m+1;i++)
-    {
-        for(int j=0;j<n+1;j++)
-        {
+    for(int i=0;i<m+1;i++){
+        for(int j=0;j<n+1;j++){
             t[i][j]=0;
         }
     }
 
-    for(int i=1;i<m+1;i++)
-    {
-        for(int j=1;j<n+1;j++)
-        {
+    for(int i=1;i<m+1;i++){
+        for(int j=1;j<n+1;j++){
             if(X[i-1]==Y[j-1])
-            t[i][j]=1+t[i-1][j-1];
+                t[i][j]=1+t[i-1][j-1];
             else
-            t[i][j]=max(t[i-1][j],t[i][j-1]);
+                t[i][j]=max(t[i-1][j],t[i][j-1]);
         }
     }
     return t[m][n];
 }
+
 //main 
 int main()
 {

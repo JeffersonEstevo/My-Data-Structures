967f7b907e9561c09c65f2a224572b0e435c2379
Jefferson Estevo <62241112+JeffersonEstevo@users.noreply.github.com>
2024-05-27 16:02:48 -0300

    Update BestTimeToBuyAndSellStock.cpp

--- C++/Algorithms/GreedyAlgorithms/BestTimeToBuyAndSellStock.cpp
+++ C++/Algorithms/GreedyAlgorithms/BestTimeToBuyAndSellStock.cpp
@@ -1,4 +1,4 @@
-You are given array prices [] which contain the price of a stock on some days. You have to choose one day for buying the stock and a different one for selling it. Return the days on which you buy and sell the stock.
+// You are given array prices [] which contain the price of a stock on some days. You have to choose one day for buying the stock and a different one for selling it. Return the days on which you buy and sell the stock.
   //author - Sushrut Shukla
   
 #include

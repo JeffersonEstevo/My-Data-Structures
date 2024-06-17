10fa54677609bc1c518dc33a2ac3b2a7f1dde53e
Jefferson Estevo <62241112+JeffersonEstevo@users.noreply.github.com>
2024-06-16 22:21:05 -0300

    Update Tower-of-Hanoi.cpp

--- C++/Algorithms/RecursionAlgorithms/Tower-of-Hanoi.cpp
+++ C++/Algorithms/RecursionAlgorithms/Tower-of-Hanoi.cpp
@@ -12,8 +12,7 @@
 using namespace std; 
   
 void towerOfHanoi(int n, char source,char destination, char intermediate)  {  
-    if (n == 1)  
-    {  
+    if (n == 1)  {  
         cout << "Move disk 1 from rod " << source <<" to rod " << destination<<endl;  
         return;  
     }  

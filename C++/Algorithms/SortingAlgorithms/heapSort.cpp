0d430b56748ba246ea4da97a857d2e423f19ff4e
Jefferson Estevo <62241112+JeffersonEstevo@users.noreply.github.com>
2024-07-19 10:31:20 -0300

    Update heapSort.cpp

--- C++/Algorithms/SortingAlgorithms/heapSort.cpp
+++ C++/Algorithms/SortingAlgorithms/heapSort.cpp
@@ -26,8 +26,7 @@ void adjust(int a[], int n, int i){ // to heapify the root
 }
 
 void heapsort(int a[], int n){
-    for (int i = n / 2 - 1; i >= 0; i--)
-    { // build heap
+    for (int i = n / 2 - 1; i >= 0; i--){ // build heap
         adjust(a, n - 1, i);
     }
 

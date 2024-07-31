8c5c50acfeb4e7d7807158e29b9d6bbe2205b24d
Jefferson Estevo <62241112+JeffersonEstevo@users.noreply.github.com>
2024-07-30 22:32:17 -0300

    Update mergeSort.cpp

--- C++/Algorithms/SortingAlgorithms/mergeSort.cpp
+++ C++/Algorithms/SortingAlgorithms/mergeSort.cpp
@@ -29,32 +29,28 @@ void merge(int arr[], int l, int m, int r) // fn for merging the elements in sor
             arr[k] = L[i];
             i++;
         }
-        else
-        {
+        else{
             arr[k] = R[j];
             j++;
         }
         k++;
     }
 
-    while (i < n1)
-    {
+    while (i < n1){
         arr[k] = L[i];
         i++;
         k++;
     }
 
-    while (j < n2)
-    {
+    while (j < n2){
         arr[k] = R[j];
         j++;
         k++;
     }
 }
-void mergeSort(int *a, int low, int high) //recursive calls for dividing array
-{
-    if (low < high)
-    {
+
+void mergeSort(int *a, int low, int high) //recursive calls for dividing array{
+    if (low < high){
         int mid = (low + high) / 2;
         mergeSort(a, low, mid);
         mergeSort(a, mid + 1, high);
@@ -62,22 +58,19 @@ void mergeSort(int *a, int low, int high) //recursive calls for dividing array
     }
 }
 
-int main()
-{
+int main(){
     int a[MAX];
     int n, num;
     cout << "Enter the number of elements in the array: ";
     cin >> n;
     cout << "Enter the elements ";
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         cin >> num;
         a[i] = num;
     }
     mergeSort(a, 0, n - 1);
     cout << "The sorted elements are : ";
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         cout << a[i] << " ";
     }
     return 0;

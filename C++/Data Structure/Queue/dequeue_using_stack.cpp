f05d0a1139bdc6ac87923353620c875f3729777d
Jefferson Estevo <62241112+JeffersonEstevo@users.noreply.github.com>
2024-09-12 14:01:01 -0300

    Update dequeue_using_stack.cpp

--- C++/Data Structure/Queue/dequeue_using_stack.cpp
+++ C++/Data Structure/Queue/dequeue_using_stack.cpp
@@ -42,14 +42,12 @@ int Dequeue(){
     }
 }
 
-int main()
-{
+int main(){
     int n, i;
     cout << "Enter the number digits to be added in queue : " << endl;
     cin >> n;
     cout << "Enter the digits : " << endl;
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++){
         int num;
         cin >> num;
         Enqueue(num);

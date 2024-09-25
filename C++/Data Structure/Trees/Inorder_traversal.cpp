5bde16066f6ac64da70092c5f39dbd4fb057b925
Jefferson Estevo <62241112+JeffersonEstevo@users.noreply.github.com>
2024-09-25 16:35:47 -0300

    Update Inorder_traversal.cpp

--- C++/Data Structure/Trees/Inorder_traversal.cpp
+++ C++/Data Structure/Trees/Inorder_traversal.cpp
@@ -33,8 +33,7 @@ binaryTree::binaryTree(){
 // Accepts a vector and calls insertKey(int) iteratively.
 void binaryTree::insertKeys(vector<int> keys){
     int len = keys.size();
-    for (int i = 0; i < len; i++)
-    {
+    for (int i = 0; i < len; i++){
         insertKey(keys[i]);
     }
 }

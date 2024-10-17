d93ae7243960c64cb67f4d7a9d0554f360e4f4bc
Jefferson Estevo <62241112+JeffersonEstevo@users.noreply.github.com>
2024-10-16 22:50:04 -0300

    Update LevelOrderTraversal.cpp

--- C++/Data Structure/Trees/LevelOrderTraversal.cpp
+++ C++/Data Structure/Trees/LevelOrderTraversal.cpp
@@ -6,7 +6,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 //implementation of binary Tree - very minimal, only used to show implementation of level order traversal
 class BinarySearchTree{
 private:

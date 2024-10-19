e545dfa68fd3e55302b620aabbe1a89bf247a0b3
Jefferson Estevo <62241112+JeffersonEstevo@users.noreply.github.com>
2024-10-18 23:11:35 -0300

    Update PostOrder_traversal.cpp

--- C++/Data Structure/Trees/PostOrder_traversal.cpp
+++ C++/Data Structure/Trees/PostOrder_traversal.cpp
@@ -29,6 +29,8 @@ binaryTree::binaryTree(){
     root = NULL;
 }
 
+
+
 // Accepts a vector and calls insertKey(int) iteratively.
 void binaryTree::insertKeys(vector<int> keys){
     int len = keys.size();
